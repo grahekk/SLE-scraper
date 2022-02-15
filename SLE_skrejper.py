cf84d8e1cb59769bcec198c65bb7ec829913cfb8
grahekk <98557014+grahekk@users.noreply.github.com>
2022-02-15 21:53:39 +0100

    Scraper update feature and formating feature in r file
    New feature for scraping small game in lgo7b list, removal of error in scraping the tables from lgo2 list and new r script for formating summary table of lgo1 form. Required packages are added as well

--- SLE_skrejper.py
+++ SLE_skrejper.py
@@ -1,11 +1,8 @@
 from bs4 import BeautifulSoup
-import requests
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import re
-import collections
 import pandas as pd
 
 base_url = "https://sle.mps.hr"
@@ -19,7 +16,7 @@ LOVISTEZAEXPORT = input("Ime lovista: ")
 search_box = driver.find_element(By.TAG_NAME, "input.form-control.input-sm")
 search_box.send_keys(LOVISTEZAEXPORT)
 
-time.sleep(0.5)
+time.sleep(1)
 loviste = driver.find_element(By.XPATH, '//*[@id="tblLovista"]/tbody/tr/td[2]/a')
 loviste.click()
 
@@ -49,20 +46,20 @@ html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 table = soup.find_all
 df = pd.read_html(str(table))[0]
-
 df.to_excel((LOVISTEZAEXPORT + "_loviste_lgo1.xlsx"))
 
-#tu krecem sa skrejpanjem podataka za divljac
-
+#clicking on the second falling list
+driver.back()
+lgo2_smjernice = driver.find_element(By.XPATH, '//*[@id="headingTwo_1"]/h4/a')
+#the number of iterations or the number of the game species
+broj_divljaci = int(re.findall('\d',lgo2_smjernice.text)[1])
+lgo2_smjernice.click()
+time.sleep(1)
+"""
+#scraping the main game data from LGO2 to table
 i = 0
-while i<4:
+while i < broj_divljaci:
     i = i+1
-
-    driver.back()
-    lgo2_smjernice = driver.find_element(By.XPATH, '//*[@id="headingTwo_1"]/h4/a')
-    lgo2_smjernice.click()
-    time.sleep(0.5)
-
     divljacpath = '//*[@id="collapseTwo_1"]/div/dd['+str(i)+']/a'
     divljac = driver.find_element(By.XPATH, divljacpath)
     divljac.click()
@@ -70,56 +67,43 @@ while i<4:
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
     podaci_divljac = soup.find_all("div", class_ = "form-group")
-    dajmi = soup.find_all("label")
 
     #tekst za vrstu divljaci
     tekst = soup.get_text(",",strip = True)
     lista = tekst.split(",")
     lista = lista[17:47]
-
     lista1 = lista[::2]
     lista2 = lista[1::2]
 
     #dobivanje tablice za vrstu divljaci
     d = {'prvi stupac':lista1, 'drugi stupac': lista2}
     df = pd.DataFrame(d)
-    df.to_excel((LOVISTEZAEXPORT + "_loviste_lgo2_"+lista[0]+".xlsx"))
+    df.to_excel((LOVISTEZAEXPORT + "_loviste_lgo2_"+lista2[0]+".xlsx"))
+
+    driver.back()
+    lgo2_smjernice = driver.find_element(By.XPATH, '//*[@id="headingTwo_1"]/h4/a')
+    time.sleep(1)
+    lgo2_smjernice.click()
+    time.sleep(1)
+"""
+lgo7b_smjernice = driver.find_element(By.XPATH, '//*[@id="headingFour_1"]/h4/a')
+lgo7b_smjernice.click()
+#again, the number of species of the small game
+broj_divljaci = int("".join(map(str, re.findall(r"\(([^)]*)\)[^(]*$", lgo7b_smjernice.text))))
+
+#list of small game species
+i=0
+sitna_divljac = []
+while i < broj_divljaci:
+    i=i+1
+    divljacpath = '//*[@id="collapseFour_1"]/div/dd['+str(i)+']/a'
+    divljac = driver.find_element(By.XPATH, divljacpath)
+    sitna_divljac.append(divljac.text.split(" / ")[1])
+    #ovo pospremiti
+
+#table of technical objects on hunting ground
+
 
 driver.back()
 print("gotof")
-
-"""
-for count,l in enumerate(lista[17:47]):
-    print(count)
-    print(l)
-    if count%2==0:
-        lista1 = lista1.append(l)
-    else:
-        lista2 = lista2.append(l)
-
-
-i = 0
-while i<12:
-    i = i+1
-    print(i)
-    divljacpath = '//*[@id="collapseTwo_1"]/div/dd[' + str(i) + ']/a'
-    print(divljacpath)
-    try:
-        divljac = driver.find_element(By.XPATH, divljacpath)
-        divljac.click()
-
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        podaci_divljac = soup.find_all("div", class_="form-group")
-
-    except:
-        print("nema vise divljaci")
-
-    driver.back()
-    lgo2_smjernice.click()
-    time.sleep(1)
-    print(i)
-"""
-
-#f.close()
 driver.close()
