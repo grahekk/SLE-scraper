910c9a311bdc98ac19c0fb43da57ee6fb1a7e80a
grahekk <98557014+grahekk@users.noreply.github.com>
2022-04-05 18:43:47 +0200

    Update SLE_skrejper.py
    All scraping features are done. The script is scraping now all the data from the web

--- SLE_skrejper.py
+++ SLE_skrejper.py
@@ -1,14 +1,19 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
 import re
 import pandas as pd
 
+start = time.time()
 base_url = "https://sle.mps.hr"
+chrome_options = Options()
+chrome_options.add_argument("--headless")
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
 driver.get(base_url)
+
 time.sleep(0.1)
 
 LOVISTEZAEXPORT = input("Ime lovista: ")
@@ -23,7 +28,7 @@ loviste.click()
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
-"""
+
 #saving the metadata for hunting grounds
 podaci_loviste = soup.find_all("div",class_ = "form-group")
 podaci_loviste_label = []
@@ -47,11 +52,11 @@ df.columns = df.iloc[0]
 df = df[1:]
 #df.to_excel((LOVISTEZAEXPORT + "_podaci o lovistu.xlsx"))
 tablica = df
-"""
+
 
 ugovor = driver.find_element(By.XPATH, '//*[@id="tblUgovori"]/tbody/tr/td[6]/a')
 ugovor.click()
-"""
+
 #contract data
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
@@ -72,8 +77,7 @@ df = pd.DataFrame(podaci_ugovor_label).transpose()
 df.columns = df.iloc[0]
 df = df[1:]
 tablica = pd.concat([tablica,df], axis=1)
-print(tablica)
-"""
+
 #df.to_excel((LOVISTEZAEXPORT + "_podaci o ugovoru.xlsx"))
 
 pregled_lgpova = driver.find_element(By.XPATH, '//*[@id="tblUgovori"]/tbody/tr/td[4]/a')
@@ -95,7 +99,7 @@ df.to_excel((LOVISTEZAEXPORT + "_loviste_lgo1.xlsx"))
 driver.back()
 lgo2_smjernice = driver.find_element(By.XPATH, '//*[@id="headingTwo_1"]/h4/a')
 #the number of iterations or the number of the game species
-broj_divljaci = int(re.findall('\d',lgo2_smjernice.text)[1])
+broj_divljaci = int(re.findall(r'\d',lgo2_smjernice.text)[1])
 lgo2_smjernice.click()
 time.sleep(1)
 
@@ -112,8 +116,7 @@ while i < broj_divljaci:
     soup = BeautifulSoup(html, 'html.parser')
     podaci_divljac = soup.find_all("div", class_ = "form-group")
 
-    ime_divljaci = driver.find_element(By.XPATH, '/html/body/section/div/div[2]/div/div/div[2]/div[1]/div[1]/h4/span')
-    print(ime_divljaci.text)
+    ime_divljaci = driver.find_element(By.XPATH, '/html/body/section/div/div[2]/div/div/div[2]/div[1]/div[1]/h4/span').text
     podaci_divljac_label = []
     for lab in podaci_divljac:
         string2 = lab.text.split("\n")
@@ -125,8 +128,14 @@ while i < broj_divljaci:
             string2[1:] = ["".join(string2[1:])]
         if "Dobna struktura" in string2:
             continue
+        string2 = [ime_divljaci + " - " + s for s in string2]
         podaci_divljac_label.append(string2)
     lgo2_divljac.append(podaci_divljac_label)
+
+    df = pd.DataFrame(podaci_divljac_label).transpose()
+    df.columns = df.iloc[0]
+    df = df[1:]
+    tablica = pd.concat([tablica, df], axis=1)
 
     #for lab in soup.find_all(name="label"):
         #podaci_loviste_label.append(lab.text)
@@ -142,8 +151,6 @@ while i < broj_divljaci:
     lgo2_smjernice.click()
     time.sleep(1)
 
-print(lgo2_divljac)
-
 lgo7b_smjernice = driver.find_element(By.XPATH, '//*[@id="headingFour_1"]/h4/a')
 lgo7b_smjernice.click()
 #again, the number of species of the small game
@@ -157,11 +164,28 @@ while i < broj_divljaci:
     divljacpath = '//*[@id="collapseFour_1"]/div/dd['+str(i)+']/a'
     divljac = driver.find_element(By.XPATH, divljacpath)
     sitna_divljac.append(divljac.text.split(" / ")[1])
-    #ovo pospremiti
+
+tablica['sitna_divljac'] = ', '.join(map(str, sitna_divljac))
 
 #table of technical objects on hunting ground
 
+lgo11_objekti = driver.find_element(By.XPATH, '//*[@id="headingFive_1"]/h4/a')
+lgo11_objekti.click()
+time.sleep(0.5)
+lgo11_objekti = driver.find_element(By.XPATH, '//*[@id="collapseFive_1"]/div/dd/a')
+lgo11_objekti.click()
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+table = soup.find_all
+df = pd.read_html(str(table))[0]
+
+df.columns = ["VRSTA OBJEKTA", "REDNI BROJ OBJEKTA", "LOKACIJA", "Z. ŠIRINA (φ)", "Z. DUŽINA (λ)"]
+df = df['VRSTA OBJEKTA'].value_counts().to_frame().transpose().rename(index={"VRSTA OBJEKTA":1})
+tablica = pd.concat([tablica, df], axis=1)
+
+tablica.to_excel("Loviste_redak_gotov.xlsx")
 
 driver.back()
-print("gotof")
+print(time.time() - start)
 driver.close()
